5628189e499787ea05cd394ef3dfea43f7ba29e0
vijai veerapandian <vijai.veerapandian@gmail.com>
2025-09-03 12:14:44 -0400

    work: udpated

--- eks-terraform-deployment/variables.tf
+++ eks-terraform-deployment/variables.tf
@@ -16,7 +16,7 @@ variable "environment" {
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "my-eks-cluster"
+  default     = "eks-cluster01"
 }
 
 variable "cluster_version" {
