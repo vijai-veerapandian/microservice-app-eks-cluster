9797a08661f480653f302874c1d88af5e28ab88f
vijai veerapandian <vijai.veerapandian@gmail.com>
2025-09-06 17:38:09 -0400

    work:eks provider for aws profile updated

--- eks-terraform-deployment/providers.tf
+++ eks-terraform-deployment/providers.tf
@@ -24,6 +24,6 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--profile", var.aws_profile]
   }
 }
